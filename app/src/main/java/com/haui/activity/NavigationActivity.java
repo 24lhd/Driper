27050ae37e3bc431915c7b3d895b74b0dec842bd
LeHongDuong <24duong@gmail.com>
2016-10-15 17:19:11 +0700

    15/10  1

---
+++ app/src/main/java/com/haui/activity/NavigationActivity.java
@@ -0,0 +1,143 @@
+package com.haui.activity;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ViewFlipper;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class NavigationActivity extends FragmentActivity implements NavigationView.OnNavigationItemSelectedListener,OnMapReadyCallback {
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setMap();
+        ViewFlipper vf = (ViewFlipper)findViewById(R.id.vf);
+        vf.setDisplayedChild(1);
+        FloatingActionButton fab = (FloatingActionButton) vf.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+//        (AppCompatActivity) setSupportActionBar(toolbar);
+//
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+         if (id == R.id.mn_user) {
+             Snackbar.make(getCurrentFocus(), "mn_user", Snackbar.LENGTH_LONG)
+                     .setAction("Action", null).show();
+        }else if (id == R.id.mn_history) {
+             Snackbar.make(getCurrentFocus(), "mn_history", Snackbar.LENGTH_LONG)
+                     .setAction("Action", null).show();
+        }else if (id == R.id.mn_nguoi_tim_xe) {
+            ViewFlipper vf = (ViewFlipper)findViewById(R.id.vf);
+            vf.setDisplayedChild(1);
+            FloatingActionButton fab = (FloatingActionButton) vf.findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+        } else if (id == R.id.mn_xe_tim_nguoi) {
+             Snackbar.make(getCurrentFocus(), "mn_nguoi_tim_xe", Snackbar.LENGTH_LONG)
+                     .setAction("Action", null).show();
+        } else if (id == R.id.mn_tai_xe) {
+             Snackbar.make(getCurrentFocus(), "mn_tai_xe", Snackbar.LENGTH_LONG)
+                     .setAction("Action", null).show();
+        } else if (id == R.id.mn_yeu_cau) {
+             Snackbar.make(getCurrentFocus(), "mn_yeu_cau", Snackbar.LENGTH_LONG)
+                     .setAction("Action", null).show();
+        } else if (id == R.id.mn_map) {
+            setMap();
+        } else if (id == R.id.mn_cai_dat) {
+             Snackbar.make(getCurrentFocus(), "mn_cai_dat", Snackbar.LENGTH_LONG)
+                     .setAction("Action", null).show();
+        }else if (id == R.id.mn_login) {
+             Snackbar.make(getCurrentFocus(), "mn_login", Snackbar.LENGTH_LONG)
+                     .setAction("Action", null).show();
+        }else if (id == R.id.mn_dev) {
+             Snackbar.make(getCurrentFocus(), "mn_dev", Snackbar.LENGTH_LONG)
+                     .setAction("Action", null).show();
+        }else if (id == R.id.mn_share) {
+             Snackbar.make(getCurrentFocus(), "mn_share", Snackbar.LENGTH_LONG)
+                     .setAction("Action", null).show();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void setMap() {
+        ViewFlipper vf = (ViewFlipper)findViewById(R.id.vf);
+        vf.setDisplayedChild(0);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    private GoogleMap mMap;
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
