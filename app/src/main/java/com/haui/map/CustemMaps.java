0e2f29980d05b977f7858e248c50d5c00c7272fb
LeHongDuong <24duong@gmail.com>
2016-11-05 18:41:25 +0700

    Đọc dữ liệu json api google map distance và các điểm location

--- app/src/main/java/com/haui/map/CustemMaps.java
+++ app/src/main/java/com/haui/map/CustemMaps.java
@@ -10,6 +10,7 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,9 +20,11 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.haui.activity.R;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -149,10 +152,6 @@ public class CustemMaps implements GoogleMap.OnMyLocationChangeListener{
             myLocation=location;
         }
     }
-    public Location getMyLocation() {
-        return myLocation;
-    }
-
     public void moveToMyLocation() {
         checkLocationIsEnable();
         if (myLocation!=null){
@@ -161,4 +160,214 @@ public class CustemMaps implements GoogleMap.OnMyLocationChangeListener{
             googleMap.animateCamera(cameraUpdate);
         }
     }
+    public Location getMyLocation() {
+        return myLocation;
+    }
+    public void drawRoadByAddress(String addressStart,String addressEnd,int color,int width){
+
+    }
+    public void drawRoadByLocation(Location locationStart,Location locationEnd,int color,int width){
+
+    }
+    public void drawRoad(ArrayList<LatLng> latLngs,int color,int width){
+        PolylineOptions options = new PolylineOptions();
+        options.color(color);;
+        options.width(width);
+        options.addAll(latLngs);
+        googleMap.addPolyline(options);
+    }
+    public void searchLocationByName(String start, String end) {
+        Location locationStart = null,locationEnd = null;
+        try {
+            List<Address> addressesStart = geocoder.getFromLocationName(start,1);
+            List<Address> addressesEnd = geocoder.getFromLocationName(end,1);
+            if (addressesStart.size()>0){
+                Address addStart = addressesStart.get(0);
+                locationStart = new Location("");
+                locationStart.setLatitude(addStart.getLatitude());
+                locationStart.setLongitude(addStart.getLongitude());
+            }
+            if (addressesEnd.size()>0){
+                Address addEnd = addressesEnd.get(0);
+                locationEnd = new Location("");
+                locationEnd.setLatitude(addEnd.getLatitude());
+                locationEnd.setLongitude(addEnd.getLongitude());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (locationStart==null||locationEnd==null){
+            Toast.makeText(context,"Vị trí không thoả mã",Toast.LENGTH_LONG).show();
+            return;
+        }else {
+            Toast.makeText(context,"Vị trí  thoả mã",Toast.LENGTH_LONG).show();
+        }
+    }
+     class GoogleMapAPI{
+        String status;
+         String place_id="place_id:";
+        String copyrights;
+        String summary;
+        String distanceText;
+        String distanceValue;
+        String durationText;
+        String durationValue;
+        String end_address;
+        String start_address;
+        String start_locationLat;
+        String start_locationLng;
+        String end_locationLat;
+        String end_locationLng;
+        ArrayList<ItemStep> itemSteps;
+
+         public ArrayList<ItemStep> getItemSteps() {
+             return itemSteps;
+         }
+
+         public GoogleMapAPI(String status, String copyrights, String summary, String distanceText, String distanceValue, String durationText, String durationValue, String end_address, String start_address, String start_locationLat, String start_locationLng, String end_locationLat, String end_locationLng, ArrayList<ItemStep> itemSteps) {
+
+             this.status = status;
+             this.copyrights = copyrights;
+             this.summary = summary;
+             this.distanceText = distanceText;
+             this.distanceValue = distanceValue;
+             this.durationText = durationText;
+             this.durationValue = durationValue;
+             this.end_address = end_address;
+             this.start_address = start_address;
+             this.start_locationLat = start_locationLat;
+             this.start_locationLng = start_locationLng;
+             this.end_locationLat = end_locationLat;
+             this.end_locationLng = end_locationLng;
+             this.itemSteps = itemSteps;
+         }
+
+         public String getStatus() {
+            return status;
+        }
+
+        public String getCopyrights() {
+            return copyrights;
+        }
+
+        public String getSummary() {
+            return summary;
+        }
+
+        public String getDistanceText() {
+            return distanceText;
+        }
+
+        public String getDistanceValue() {
+            return distanceValue;
+        }
+
+        public String getDurationText() {
+            return durationText;
+        }
+
+        public String getDurationValue() {
+            return durationValue;
+        }
+
+        public String getEnd_address() {
+            return end_address;
+        }
+
+        public String getStart_address() {
+            return start_address;
+        }
+
+        public String getStart_locationLat() {
+            return start_locationLat;
+        }
+
+        public String getStart_locationLng() {
+            return start_locationLng;
+        }
+
+        public String getEnd_locationLat() {
+            return end_locationLat;
+        }
+
+        public String getEnd_locationLng() {
+            return end_locationLng;
+        }
+    }
+     class ItemStep{
+        String distanceTextSteps;
+        String distanceValueSteps;
+        String durationTextSteps;
+        String durationValueSteps;
+        String start_locationLatSteps;
+        String start_locationLngSteps;
+        String end_locationLatSteps;
+        String end_locationLngSteps;
+        String html_instructions;
+        String travel_mode;
+
+        public String getDistanceTextSteps() {
+            return distanceTextSteps;
+        }
+
+        public String getDistanceValueSteps() {
+            return distanceValueSteps;
+        }
+
+        public String getDurationTextSteps() {
+            return durationTextSteps;
+        }
+
+        public String getDurationValueSteps() {
+            return durationValueSteps;
+        }
+
+        public String getStart_locationLatSteps() {
+            return start_locationLatSteps;
+        }
+
+        public String getStart_locationLngSteps() {
+            return start_locationLngSteps;
+        }
+
+        public String getEnd_locationLatSteps() {
+            return end_locationLatSteps;
+        }
+
+        public String getEnd_locationLngSteps() {
+            return end_locationLngSteps;
+        }
+
+        public String getHtml_instructions() {
+            return html_instructions;
+        }
+
+        public String getTravel_mode() {
+            return travel_mode;
+        }
+
+        public ItemStep(String distanceTextSteps,
+                        String distanceValueSteps,
+                        String durationTextSteps,
+                        String durationValueSteps,
+                        String start_locationLatSteps,
+                        String start_locationLngSteps,
+                        String end_locationLatSteps,
+                        String end_locationLngSteps,
+                        String html_instructions,
+                        String travel_mode) {
+            this.distanceTextSteps = distanceTextSteps;
+            this.distanceValueSteps = distanceValueSteps;
+            this.durationTextSteps = durationTextSteps;
+            this.durationValueSteps = durationValueSteps;
+            this.start_locationLatSteps = start_locationLatSteps;
+            this.start_locationLngSteps = start_locationLngSteps;
+            this.end_locationLatSteps = end_locationLatSteps;
+            this.end_locationLngSteps = end_locationLngSteps;
+            this.html_instructions = html_instructions;
+            this.travel_mode = travel_mode;
+
+        }
+    }
+
 }
